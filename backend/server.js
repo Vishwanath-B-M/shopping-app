8e21f0d966e68347dc0ba7a9041fd0c93b5056f0
Vishwanath-B-M <vishwanath@Vishwanaths-MacBook-Air.local>
2026-05-28 17:03:36 +0530

    removed all

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config()
-const app=require("./src/app")
-const connectiondb=require("./src/database/db")
-connectiondb()
-
-app.listen(3000,()=>{
-    console.log('server is running in port number 3000')
-})
--- backend/src/routers/auth.router.js
+++
@@ -1,5 +0,0 @@
-const express=require('express')
-const authController=require("../controller/auth.controller")
-const router=express.Router()
-router.post("/singup",authController.singup)
-module.exports=router
