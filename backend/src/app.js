16d8f1060018d859f55f9ae49e04df4fe385ba21
Vishwanath-B-M <vishwanath@Vishwanaths-MacBook-Air.local>
2026-05-28 16:39:35 +0530

    added

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const cookieparser=require('cookie-parser')
+const cors=require('cors')
+const authrouter=require('./routers/auth.router')
+const app=express()
+app.use(express.json())
+app.use(cookieparser())
+app.use(cors())
+app.use("/api/auth",authrouter)
+module.exports=app;
